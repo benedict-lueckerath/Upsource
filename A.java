459b2cbc4b7db31b1ab78c4b053a2bcb9570d210
Alexander Rosenberg <alexander.rosenberg@eudemonia-solutions.de>
2022-03-31 11:35:25 +0200

    Zweite Klasse

---
+++ A.java
@@ -0,0 +1,12 @@
+public class A
+{
+    private String ooo;
+
+    public String getOoo() {
+        return ooo;
+    }
+
+    public void setOoo(String ooo) {
+        this.ooo = ooo;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        A a = new A();
+        a.setOoo("OOO");
+        System.out.println(a.getOoo());
     }
 }
